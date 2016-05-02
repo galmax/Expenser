4e12737f2ee2aaa72df847952fb90cd8172d10fc
galmax <maks10059@mail.ru>
2016-05-02 21:19:58 +0300

    create statistic activity

--- app/src/main/java/com/github/sinapple/expenser/statistic/StatisticActivity.java
+++ app/src/main/java/com/github/sinapple/expenser/statistic/StatisticActivity.java
@@ -1,7 +1,18 @@
 package com.github.sinapple.expenser.statistic;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+import com.github.sinapple.expenser.R;
+
 /**
  * Created by Jarvis on 02.05.2016.
  */
-public class StatisticActivity {
+public class StatisticActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_statistic);
+    }
 }
