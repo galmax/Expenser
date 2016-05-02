df954b4b86055f1e9a50b652c8ada0e49d71c23d
DistortionEmotion <julliayashchuk@gmail.com>
2016-05-02 23:50:06 +0300

    Add swipe to delete, click to edit, input validation for categories

--- app/src/main/java/com/github/sinapple/expenser/CategoryActivity.java
+++ app/src/main/java/com/github/sinapple/expenser/CategoryActivity.java
@@ -4,10 +4,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.Menu;
 import android.view.View;
 import android.widget.TextView;
@@ -29,14 +31,17 @@ TextView incomeListLabel,expenseListLabel;
             @Override
             public void onClick(View view) {
                 Intent in = new Intent(CategoryActivity.this, NewCategoryActivity.class);
-                in.putExtra("action", false);
+                in.putExtra("edit_category", false);
                 startActivity(in);
             }
         });
 
-        //set up adapters for income and expense lists
-        CustomCatListAdapter expenseListAdapter = new CustomCatListAdapter(TransactionCategory.find(TransactionCategory.class, "m_expense_category = ?", "1"));
-        CustomCatListAdapter incomeListAdapter = new CustomCatListAdapter(TransactionCategory.find(TransactionCategory.class,"m_expense_category = ?","0"));
+        //set up adapters and listeners for income and expense lists
+        CustomCatListAdapter expenseListAdapter = new CustomCatListAdapter(getApplicationContext(),TransactionCategory.find(TransactionCategory.class, "m_expense_category = ?", "1"));
+        CustomCatListAdapter incomeListAdapter = new CustomCatListAdapter(getApplicationContext(),TransactionCategory.find(TransactionCategory.class,"m_expense_category = ?","0"));
+        RecycleItemClickListener expClickListener = new RecycleItemClickListener(this.getApplicationContext(), expenseListAdapter);
+        RecycleItemClickListener incClickListener = new RecycleItemClickListener(this.getApplicationContext(), incomeListAdapter);
+
         RecyclerView expenseCategoryList = (RecyclerView)findViewById(R.id.expenseList);
         expenseCategoryList.setLayoutManager(new LinearLayoutManager(this));
         expenseCategoryList.setAdapter(expenseListAdapter);
@@ -44,11 +49,21 @@ TextView incomeListLabel,expenseListLabel;
         incomeCategoryList.setLayoutManager(new LinearLayoutManager(this));
         incomeCategoryList.setAdapter(incomeListAdapter);
 
+        expenseCategoryList.addOnItemTouchListener(expClickListener);
+        ItemTouchHelper.Callback callbackExp = new RecyclerViewItemCallback(expenseListAdapter, ContextCompat.getColor(this, R.color.colorAccent));
+        ItemTouchHelper helperExp = new ItemTouchHelper(callbackExp);
+        helperExp.attachToRecyclerView(expenseCategoryList);
+
+        incomeCategoryList.addOnItemTouchListener(incClickListener);
+        ItemTouchHelper.Callback callbackInc = new RecyclerViewItemCallback(incomeListAdapter, ContextCompat.getColor(this, R.color.colorAccent));
+        ItemTouchHelper helperInc = new ItemTouchHelper(callbackInc);
+        helperInc.attachToRecyclerView(incomeCategoryList);
+
         //check the total number of items in lists and change the textViews accordingly
         if (expenseListAdapter.getItemCount()==0 && incomeListAdapter.getItemCount()==0) {
             expenseListLabel.setText("There are no categories to display. Please press the \"+\" button to add a new category.");
             //don't display the label below
-            incomeListLabel.setText("");
+            incomeListLabel.setVisibility(View.GONE);
         } else if (expenseListAdapter.getItemCount()==0){
             expenseListLabel.setText("There are no expense categories to display. Please press the \"+\" button to add a new category.");
         } else if (incomeListAdapter.getItemCount()==0){
--- app/src/main/java/com/github/sinapple/expenser/CustomCatListAdapter.java
+++ app/src/main/java/com/github/sinapple/expenser/CustomCatListAdapter.java
@@ -1,15 +1,21 @@
 package com.github.sinapple.expenser;
 
+import android.content.Context;
+import android.content.Intent;
+import android.support.design.widget.Snackbar;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
 import com.github.sinapple.expenser.model.TransactionCategory;
+import com.github.sinapple.expenser.model.MoneyTransaction;
 import java.util.List;
 
-public class CustomCatListAdapter extends RecyclerView.Adapter<CustomCatListAdapter.ViewHolder>{
+public class CustomCatListAdapter extends RecyclerView.Adapter<CustomCatListAdapter.ViewHolder> implements RecyclerViewItemCallback.ItemTouchHelperAdapter, RecycleItemClickListener.OnItemClickListener{
     List<TransactionCategory> mCategories;
+    Context mContext;
     public class ViewHolder extends RecyclerView.ViewHolder{
         public TextView mTitle;
         public TextView mDescription;
@@ -20,7 +26,8 @@ public class CustomCatListAdapter extends RecyclerView.Adapter<CustomCatListAdap
             mDescription = (TextView)itemView.findViewById(R.id.description_category);
         }
     }
-    public CustomCatListAdapter(List<TransactionCategory> categories) {
+    public CustomCatListAdapter(Context activityContext,List<TransactionCategory> categories) {
+        mContext = activityContext;
         mCategories = categories;
     }
 
@@ -40,5 +47,45 @@ public class CustomCatListAdapter extends RecyclerView.Adapter<CustomCatListAdap
     }
     public int getItemCount() {
         return mCategories.size();
+    }
+
+    //Remove some item. Usually called when the swipe gesture has been performed.
+    @Override
+    public void onItemDismiss(View messageOutput, int position){
+        final int p = position;
+        final TransactionCategory x = mCategories.remove(position);
+        final List<MoneyTransaction> mTransactions = MoneyTransaction.find(MoneyTransaction.class, "m_category = ?", x.getId().toString());
+        notifyDataSetChanged();
+        Snackbar.make(messageOutput, R.string.message_category_deleted, Snackbar.LENGTH_SHORT)
+                .setAction(R.string.cancel, new View.OnClickListener() {
+                    //Return deleted category to the list if the user has canceled removing
+                    @Override
+                    public void onClick(View v) {
+                        mCategories.add(p, x);
+                        notifyItemInserted(p);
+                    }
+                })
+                .setCallback(new Snackbar.Callback() {
+                    //Final removing of the category and all transactions in it
+                    @Override
+                    public void onDismissed(Snackbar snackbar, int event) {
+                        if (event != Snackbar.Callback.DISMISS_EVENT_ACTION) {
+                            x.delete();
+                            for (MoneyTransaction m:mTransactions) {
+                                m.delete();
+                            }
+                        }
+                        super.onDismissed(snackbar, event);
+                    }
+                }).show();
+    }
+
+    @Override
+    public void onItemClick(View view, int position) {
+        Intent editIntent = new Intent(view.getContext(), NewCategoryActivity.class);
+        editIntent.putExtra("edit_category", true);
+        editIntent.putExtra("id", mCategories.get(position).getId());
+        editIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        mContext.startActivity(editIntent);
     }
 }
