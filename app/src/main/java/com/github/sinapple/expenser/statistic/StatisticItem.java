42d16d8ef051f0b226adf58abe936655e2488f2b
galmax <maks10059@mail.ru>
2016-05-05 07:20:04 +0300

    refactor StatisticAdapder.java, StatisticItem.java

--- app/src/main/java/com/github/sinapple/expenser/statistic/StatisticItem.java
+++ app/src/main/java/com/github/sinapple/expenser/statistic/StatisticItem.java
@@ -1,7 +1,44 @@
 package com.github.sinapple.expenser.statistic;
 
+import com.github.sinapple.expenser.model.TransactionCategory;
+
 /**
  * Created by Jarvis on 04.05.2016.
  */
 public class StatisticItem {
+    private TransactionCategory category;
+    private float amount;
+    private Integer color;
+
+    public StatisticItem(){}
+
+    public StatisticItem(TransactionCategory category, float amount, Integer color) {
+        this.category = category;
+        this.amount = amount;
+        this.color = color;
+    }
+
+    public TransactionCategory getCategory() {
+        return category;
+    }
+
+    public void setCategory(TransactionCategory category) {
+        this.category = category;
+    }
+
+    public float getAmount() {
+        return amount;
+    }
+
+    public void setAmount(float amount) {
+        this.amount = amount;
+    }
+
+    public Integer getColor() {
+        return color;
+    }
+
+    public void setColor(Integer color) {
+        this.color = color;
+    }
 }
