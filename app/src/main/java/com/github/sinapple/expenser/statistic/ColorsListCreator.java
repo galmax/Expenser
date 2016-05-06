1210ea7a5803197333a71e7f58128dd30b39b89e
galmax <maks10059@mail.ru>
2016-05-06 08:01:18 +0300

    add getColors method, which get colors list

--- app/src/main/java/com/github/sinapple/expenser/statistic/ColorsListCreator.java
+++ app/src/main/java/com/github/sinapple/expenser/statistic/ColorsListCreator.java
@@ -1,7 +1,32 @@
 package com.github.sinapple.expenser.statistic;
 
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Jarvis on 05.05.2016.
  */
 public class ColorsListCreator {
+    private List<Integer> colors;
+
+    public ColorsListCreator() {
+        colors = new ArrayList<>();
+        for (int c : ColorTemplate.COLORFUL_COLORS)
+            colors.add(c);
+        for (int c : ColorTemplate.LIBERTY_COLORS)
+            colors.add(c);
+        for (int c : ColorTemplate.PASTEL_COLORS)
+            colors.add(c);
+        for (int c : ColorTemplate.VORDIPLOM_COLORS)
+            colors.add(c);
+        for (int c : ColorTemplate.JOYFUL_COLORS)
+            colors.add(c);
+        colors.add(ColorTemplate.getHoloBlue());
+    }
+
+    public List<Integer> getColors() {
+       return colors;
+    }
 }
